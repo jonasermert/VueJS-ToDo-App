d39a71a9d98b1eaaa885baf05ba64f5b7b26df03
Jonas <ermert.jonas@yahoo.de>
2022-07-11 10:43:16 +0200

    added delete function

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -1,18 +1,19 @@
 <template>
-  <div >
+  <div class="flex justify-between items-center py-2 px-2" :class="{'bg-green-600': this.todo.done, 'bg-red-600: !todo.todo.done'}" >
     <!--
     <h1>{{message}}</h1>
     <h1>{{count}}</h1>
     <button v-on:click="countUp">Hochzählen</button>
     -->
-    <h1>{{todo}}</h1>
+    <h1 class="cursor-pointer" @click="toggleToDo">{{todo.todo}}</h1>
+    <button class="bg-black py-1 px-1 rounded" @click="removeToDo">Delete ToDo</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'ToDo',
-  props: ["todoprop"],
+  props: ["todoprop", "todoindex"],
   data: function() {
     return {
       /*
@@ -28,6 +29,16 @@ export default {
       this.count++;
     }
     */
+    toggleToDo(){
+      // console.log(this.todoindex);
+      this.$emit("toggleDone-index", this.todoindex);
+    },
+    removeToDo() {
+      this.$emit("removetodo-index", this.todoindex);
+    }
+
+
+
   }
 }
 </script>
