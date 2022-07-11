fbe06ed1c84c0c5152544781d4cf581d0b5405b8
Jonas <ermert.jonas@yahoo.de>
2022-07-11 10:15:59 +0200

    added styles, added ToDo's to list

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,22 @@
 <template>
-  <ToDo />
+  <div class="bg-gray-600 text-white w-1/2 m-auto mt-12">
+    <div class="text-center border-b-2 border-black py-4">
+      <h1 class="text-3xl py-4">Meine ToDo's</h1>
+      <p class="text-xl">Offene ToDo's: {{todos.length}}</p>
+    </div>
+    
+    <div v-for="todo in todos" :key="todo.todo">
+      <ToDo :todoprop="todo.todo"/>
+      <!--
+      <ToDo :todoprop="todos[0].todo"/>
+      <ToDo :todoprop="todos[1].todo"/>
+      <ToDo :todoprop="todos[2].todo"/>
+      -->
+    </div>
+
+
+  </div>
+  
 </template>
 
 <script>
@@ -7,6 +24,15 @@ import ToDo from './components/ToDo.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      todos: [
+        {todo:"programming", done: false},
+        {todo:"shopping", done: false},
+        {todo:"sport", done: false},
+      ],
+    }
+  },
   components: {
     ToDo,
   }
--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -1,24 +1,33 @@
 <template>
   <div >
+    <!--
     <h1>{{message}}</h1>
     <h1>{{count}}</h1>
     <button v-on:click="countUp">Hochzählen</button>
+    -->
+    <h1>{{todo}}</h1>
   </div>
 </template>
 
 <script>
 export default {
   name: 'ToDo',
+  props: ["todoprop"],
   data: function() {
     return {
+      /*
       message: "Hallo ToDo",
       count: 0,
-    }
+      */
+     todo: this.todoprop,
+    };
   },
   methods: {
+    /*
     countUp(){
       this.count++;
     }
+    */
   }
 }
 </script>
