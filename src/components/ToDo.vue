e1d7f2ae728a0ebf3c68b340a4f65f44086eb869
Jonas <ermert.jonas@yahoo.de>
2022-07-11 09:57:53 +0200

    project tidied up, installed tailwind, tested - ok

---
+++ src/components/ToDo.vue
@@ -0,0 +1,25 @@
+<template>
+  <div >
+    <h1>{{message}}</h1>
+    <h1>{{count}}</h1>
+    <button v-on:click="countUp">Hochzählen</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ToDo',
+  data: function() {
+    return {
+      message: "Hallo ToDo",
+      count: 0,
+    }
+  },
+  methods: {
+    countUp(){
+      this.count++;
+    }
+  }
+}
+</script>
+
